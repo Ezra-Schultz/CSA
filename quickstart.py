ef12397146091886e6b5f4a243a70c6653c1a6c2
CSA10158 <schultzezra8@gmail.com>
2022-10-01 14:33:58 -0500

    Fixed OAuth errors completely

--- quickstart.py
+++ quickstart.py
@@ -7,7 +7,6 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
@@ -31,9 +30,8 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080, )
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
