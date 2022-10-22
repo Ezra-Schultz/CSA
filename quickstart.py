b50dbb5f6bdc9fa1c4b79c34b010c3d480033b31
CSA10158 <schultzezra8@gmail.com>
2022-10-22 13:47:51 -0500

    Fixed error in getting data + optimised

--- quickstart.py
+++ quickstart.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
+import sys
+import subprocess
 
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gspread'])
 import os.path
 import gspread
 
@@ -18,7 +21,8 @@ SAMPLE_SPREADSHEET_URL = input("Enter spreadsheet URL: ")
 SAMPLE_SPREADSHEET_ID = SAMPLE_SPREADSHEET_URL.strip(
     "https://docs.google.com/spreadsheets/d/"
 )[:-11]
-SAMPLE_RANGE_NAME = "A1:D30"
+SAMPLE_SPREADSHEET_ID = '1d0hwrPTGWjzQ60HL6bBy2olGi-lg20aozqeEWTM0Gjw'
+SAMPLE_RANGE_NAME = "A1:C30"
 
 
 def curve(scores: list):
@@ -54,33 +58,42 @@ def main():
     try:
         # service = build("sheets", "v4", credentials=creds)
 
-        # sheet = service.spreadsheets()
         client = gspread.authorize(creds)
         sheet = client.open("Grades").sheet1
 
+        range_ = sheet.range('A1:C30')
+        formatList = []
+        for j in range(3):
+            tempList = []
+            for i in range(len(range_)):
+                if i % 3 == j:
+                    tempList.append(range_[i].value)
+            formatList.append(tempList)
+        [print(i) for i in formatList]
         # writeCurve = sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID, range='D2:D30')
-        count = 1
-        lst = []
-        while sheet.row_values(count) != []:
-            values = sheet.row_values(count)
-            lst.append(values)
-            count += 1
-        print(lst)
+        # count = 1
+        # lst = []
+        # while sheet.row_values(count):
+        #     values = sheet.row_values(count)
+        #     lst.append(values)
+        #     count += 1
+        # print(lst)
         # if not values:
         # print("No data found.")
         # return
-        cells = sheet.range(SAMPLE_RANGE_NAME)
-        print([cell.value for cell in cells])
-        for i in range(0, len(cells), 4):
-            yield cells[i : i + 4]
+        # cells = sheet.range('A1:B7')
+        # for i in cells:
+        #     print(i)
+        # for i in range(0, len(cells), 4):
+        #     yield cells[i: i + 4]
 
         # curveGrades = curve([int(row[2]) for row in values])
         # sheet.update_acell(4, 2, curveGrades[1])
-        for i in range(len(values)):
-            data = values[i]
-            print(
-                f"{data[0]} {data[1]} -> grade: {data[2]}"
-            )
+        # for i in range(len(values)):
+        #     data = values[i]
+        #     print(
+        #         f"{data[0]} {data[1]} -> grade: {data[2]}"
+        #     )
 
     except Exception as err:
         print(err)
